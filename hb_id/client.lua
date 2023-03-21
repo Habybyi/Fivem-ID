f4d353258ad47b58b9b551d6dc75e877078b67b3
Habybyi <128488292+Habybyi@users.noreply.github.com>
2023-03-21 16:37:20 +0100

    Add files via upload

---
+++ hb_id/client.lua
@@ -0,0 +1,15 @@
+local Player_Id = GetPlayerServerId(NetworkGetPlayerIndexFromPed(PlayerPedId()))
+
+RegisterCommand("id",function(source,args,rawCommand)
+    Notif("Your server ID is: "..GetPlayerServerId(NetworkGetPlayerIndexFromPed(PlayerPedId())))
+end)
+
+function Notif(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawNotification(false, false)
+end
+
+-- function Notif(text)
+--     exports['okokNotify']:Alert("Player ID: "..Player_Id, text, 3000, 'warning')
+-- end
---
+++ hb_id/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version  'adamant'
+game  'gta5'
+
+author 'Habybyi'
+desciption 'Script for IF (okok notify)'
+version '1.0'
+
+client_scripts {
+    'client.lua'
+}
